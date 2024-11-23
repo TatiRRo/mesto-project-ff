11e491e52765f3dc850521a73faa8835c3f621cc
Маргарита Рождественская <m.rogdestvenskya@mail.ru>
2024-11-23 17:26:23 +0100

    feat: add adding and deleting cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+initialCards.forEach(({ name, link }) => {
+    const cardItem = cardTemplate
+        .querySelector('.places__item')
+        .cloneNode(true);
+    const cardImage = cardItem.querySelector('.card__image');
+    const cardImageTitle = cardItem.querySelector('.card__title');
 
-// @todo: Функция создания карточки
+    cardImage.src = link;
+    cardImageTitle.textContent = name;
 
-// @todo: Функция удаления карточки
+    cardList.append(cardItem);
+});
 
-// @todo: Вывести карточки на страницу
+const addCardButton = document.querySelector('.profile__add-button');
+const popupAddPlace = document.querySelector('.popup_type_new-card');
+
+addCardButton.addEventListener('click', function () {
+    popupAddPlace.style.display = 'flex';
+});
+
+const closePopUpdButton = popupAddPlace.querySelector('.popup__close');
+
+closePopUpdButton.addEventListener('click', function () {
+    popupAddPlace.style.display = 'none';
+});
+
+cardList.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains('card__delete-button')) {
+        const cardItem = evt.target.closest('.places__item');
+        if (cardItem) {
+            cardItem.remove();
+        }
+    }
+});
